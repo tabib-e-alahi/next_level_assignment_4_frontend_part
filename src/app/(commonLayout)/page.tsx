9211dfa127f8b543ced904620547a7efe647bfc5
tabib-e-alahi <etabib.alahi@gmail.com>
2026-03-19 23:38:02 +0600

    \`Refactor proxy.ts for role-based routing\`

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -2,9 +2,11 @@ import Hero from "@/components/modules/home/Hero";
 import TopCategorySection from "@/components/modules/home/TopCategorySection";
 import TopProvidersSection from "@/components/modules/home/TopProviderSection";
 import WhyPlatera from "@/components/modules/home/WhyPlatera";
+import { getUser } from "@/services/auth";
 
-export default function HomePage() {
- 
+export default async function HomePage() {
+ const userData = await getUser()
+ console.log(userData)
   return (
     <div>
       <Hero></Hero>
