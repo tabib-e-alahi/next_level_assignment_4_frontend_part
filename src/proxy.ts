a23e3cdbdaa65fa28c27f16b4a38755b009796e2
tabib-e-alahi <etabib.alahi@gmail.com>
2026-03-29 23:07:26 +0600

    Refactor proxy.ts for role-based routing

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,11 +1,7 @@
 import { NextRequest, NextResponse } from "next/server"
 import { getUser } from "@/services/auth"
-import { Role, Roles } from "@/constants/roles";
+import { Role } from "@/constants/roles";
 
-
-
-// const PUBLIC_ROUTES = ["/", "/login", "/register", "/meals", "/meals/:path*"]
-// const AUTH_ROUTES = ["/login", "/register"]
 
 const ROLE_BASED_ROUTES: { route: string; roles: Role[] }[] = [
   {
@@ -38,39 +34,37 @@ export async function proxy(request: NextRequest) {
   console.log("Form proxy-----: ", userData);
 
   const isAuthenticated = !!userData
-  const userRole = userData?.role as Role | undefined
 
   // if the user is not authenticated then will redirect to login
-  if(!isAuthenticated){
+  if (!isAuthenticated) {
     return NextResponse.redirect(new URL("/login", request.url))
   }
-  
+
+  const userRole = userData?.role as Role | undefined
+
   // Check protected routes
   const matchedRoute = ROLE_BASED_ROUTES.find((routeObj) =>
     pathname.startsWith(routeObj.route)
   )
 
-  // If route is protected and role is not allowed
-  // if (matchedRoute && isAuthenticated) {
-  //   const isAllowed = matchedRoute.roles.includes(userRole as Role)
+  if (matchedRoute && isAuthenticated) {
+    const isAllowed = matchedRoute!.roles.includes(userRole as Role)
 
-  //   if (!isAllowed) {
-  //     // Redirect user based on their own role
-  //     if (userRole === "ADMIN") {
-  //       return NextResponse.redirect(new URL("/admin-dashboard", request.url))
-  //     }
+    if (!isAllowed && userRole === "ADMIN") {
+      return NextResponse.redirect(new URL("/admin-dashboard", request.url))
+    }
 
-  //     if (userRole === "PROVIDER") {
-  //       return NextResponse.redirect(new URL("/provider-dashboard", request.url))
-  //     }
+    if (!isAllowed && userRole === "PROVIDER") {
+      return NextResponse.redirect(new URL("/provider-dashboard", request.url))
+    }
 
-  //     if (userRole === "CUSTOMER") {
-  //       return NextResponse.redirect(new URL("/", request.url))
-  //     }
+    if (!isAllowed && userRole === "CUSTOMER") {
+      return NextResponse.redirect(new URL("/customer-dashboard", request.url))
+    }
 
-  //     return NextResponse.redirect(new URL("/login", request.url))
-  //   }
-  // }
+    return NextResponse.redirect(new URL("/login", request.url))
+  }
+
 
   return NextResponse.next()
 }
@@ -79,8 +73,8 @@ export const config = {
   matcher: [
     "/cart",
     // "/checkout",
-    // "/customer-dashboard/:path*",
-    // "/provider-dashboard/:path*",
-    // "/admin-dashboard/:path*",
+    "/customer-dashboard/:path*",
+    "/provider-dashboard/:path*",
+    "/admin-dashboard/:path*",
   ],
 }
